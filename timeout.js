9e8d83e3fdebe4f67d23369fa14bc002289c6a97
RianIslam <rianislamdr@gmail.com>
2021-02-05 19:07:59 +0600

    setInterval

--- timeout.js
+++ timeout.js
@@ -3,7 +3,11 @@ function doSomething(){
 }
 console.log(334343);
 console.log(334343);
-setTimeout( doSomething, 10000);
+// setTimeout( doSomething, 10000);
 console.log(334343);
+
+setInterval(function(){
+    console.log('doit')
+},10000)
 console.log(334343);
 console.log(334343);
