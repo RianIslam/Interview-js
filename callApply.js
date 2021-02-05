fbe5a5acae8980021241eca553128f5685aac83a
RianIslam <rianislamdr@gmail.com>
2021-02-05 15:25:53 +0600

    this

--- callApply.js
+++ callApply.js
@@ -1,10 +1,17 @@
 const normalPerson = {
     firstName : "rahim",
     lastName : "uddin",
+    salary :"1300",
     getFullName : function(){
         console.log(this.firstName,this.lastName);
+    }
+    ,
+    chargeBill : function(amount){
+        this.salary = this.salary - amount;
+        return this.salary;
     }
 
 }
 
-console.log(normalPerson)
+normalPerson.chargeBill(300);
+console.log(normalPerson.salary);
