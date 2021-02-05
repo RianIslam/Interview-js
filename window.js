fc4eced97b05f793b18cce262ad2c7b9b0fb5609
RianIslam <rianislamdr@gmail.com>
2021-02-05 17:20:41 +0600

    window.html

--- window.js
+++ window.js
@@ -2,7 +2,11 @@ var name = "wow";
 function add(num1,num2){
     var result = num1 + num2;
     console.log('inside the function', name)
-    return result;
+    function double(num){
+        return num *2 ;
+    }
+    var total = double(result)
+    return total;
     
 }
 console.log('outside the function', name);
