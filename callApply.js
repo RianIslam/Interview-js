73fc8b50bf47f2f342e98327e2c4551f08a87468
RianIslam <rianislamdr@gmail.com>
2021-02-05 15:16:20 +0600

    this call

---
+++ callApply.js
@@ -0,0 +1,10 @@
+const normalPerson = {
+    firstName : "rahim",
+    lastName : "uddin",
+    getFullName : function(){
+        console.log(this.firstName,this.lastName);
+    }
+
+}
+
+console.log(normalPerson)
