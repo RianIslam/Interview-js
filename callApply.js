1402a75ebc41efc772de6fd0cf971498f3bf9f92
RianIslam <rianislamdr@gmail.com>
2021-02-05 16:15:29 +0600

    bind

--- callApply.js
+++ callApply.js
@@ -7,12 +7,30 @@ const normalPerson = {
     }
     ,
     chargeBill : function(amount){
+        console.log(this);
         this.salary = this.salary - amount;
         return this.salary;
     }
 
 }
-const result = normalPerson.getFullName;
-console.log(result);
-normalPerson.chargeBill(300);
-console.log(normalPerson.salary);
+// const result = normalPerson.getFullName;
+// console.log(result);
+// normalPerson.chargeBill(300);
+// console.log(normalPerson.salary);
+
+
+const heroPerson ={
+    firstName : 'hero',
+    lastName : 'balam',
+    salary : '1200'
+}
+
+const heroPersonBill ={
+    firstName : 'hero',
+    lastName : 'Alom',
+    salary : '1200'
+}
+
+const heroChargBill = normalPerson.chargeBill.bind(heroPerson);
+heroChargBill(300);
+
