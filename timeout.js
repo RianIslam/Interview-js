80be1b381ecd8412894b1659921bd50b2a2ec614
RianIslam <rianislamdr@gmail.com>
2021-02-05 19:04:14 +0600

    setTimeout

---
+++ timeout.js
@@ -0,0 +1,9 @@
+function doSomething(){
+    console.log(3333);
+}
+console.log(334343);
+console.log(334343);
+setTimeout( doSomething, 10000);
+console.log(334343);
+console.log(334343);
+console.log(334343);
