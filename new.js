aceb8fe373eba25bc0d9ec3938982bb43c1b650a
RianIslam <rianislamdr@gmail.com>
2021-02-05 17:33:08 +0600

    constructor

---
+++ new.js
@@ -0,0 +1,10 @@
+class Person{
+    constructor(firstName,lastName,salary){
+        this.fistName = firstName
+        this.lastName = lastName
+        this.salary = salary;
+    }
+}
+
+const heroPerson = new Person("hero","hashu","599");
+console.log(heroPerson);
