cf1e4fb677bbbf49b983772a7a6869b52a9369b0
RianIslam <rianislamdr@gmail.com>
2021-02-05 17:42:00 +0600

    constructor class

--- new.js
+++ new.js
@@ -8,3 +8,11 @@ class Person{
 
 const heroPerson = new Person("hero","hashu","599");
 console.log(heroPerson);
+
+// function Person1(firstName,lastName,salary){
+//     this.fistName  = firstName;
+//     this.lastName = lastName;
+//     this.salary = salary;
+// }
+// const oldPerson = new Person1('grand','papa',1200)
+// console.log(oldPerson)
