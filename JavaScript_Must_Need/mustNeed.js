122873be8cf07221d16341b7427dab9b4fe15b77
RianIslam <rianislamdr@gmail.com>
2021-02-15 20:51:10 +0600

    need basic

---
+++ JavaScript_Must_Need/mustNeed.js
@@ -0,0 +1,45 @@
+let money = 20;
+money = 39;
+const name = 'kuddus';
+const person ={
+    name = 'kuddus',
+    age = 44,
+    city = 'Dhaka'
+}
+const statement = `this person ${name} with age ${age}`
+
+//condition
+
+if(money > 49 && age >=38){
+
+}
+else{
+
+}
+
+//array
+
+const number = [35,56,78];
+const respected = ['motiur','jahangir'];
+const  preoduct = [{name: 'laptop'},{name:'motiur'},]
+
+//loop::for loop
+
+for(let i= 0;i<numbers.length;i++){
+    const number = numbers[i];
+}
+
+//function :: regular arrow
+
+function add (num1,num2){
+    return num2+ num1
+}
+const result = add(38,45);
+
+//arrow
+
+const add3 = (num) =>num*3;
+const doMath = (x,y)=>{
+    const sum = x+y;
+    return sum*4;
+}
