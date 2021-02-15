c7be82c9c0865963d615e54cfd1985b67b4a78d0
RianIslam <rianislamdr@gmail.com>
2021-02-15 21:45:00 +0600

    add some code

---
+++ JavaScript_Must_Need/arraMustNeed.js
@@ -0,0 +1,12 @@
+const numbers = [3,56,97,56,967];
+const friends = ['rafiq','jabbar','barkat'];
+const products = [
+    {id:1,name:'laptop',price:8485},
+    {id:2,name:'mobile',price:244},
+    {id:3,name:'watch',price:533},
+    {id:4,name:'tablet',price:245}
+];
+
+//map
+const names = products.map(product => product.name);
+console.log(names);
