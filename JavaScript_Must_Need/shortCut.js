874fb5e4434de38aa09e76594aa1f0b3e2880773
RianIslam <rianislamdr@gmail.com>
2021-02-18 01:18:42 +0600

    shortCut

--- JavaScript_Must_Need/shortCut.js
+++ JavaScript_Must_Need/shortCut.js
@@ -1,29 +1,43 @@
-// const money = 348;
-// const user =true;
-// let food;
-// if(money>100){
-//     food = 'biriany'
-// }else{
-//     food="alu vorta"
-// }
-// //ternary
-// let food = money >100 ? 'biriany' : 'alur vorta'
+const money = 348;
+const user =true;
+let food;
+if(money>100){
+    food = 'biriany'
+}else{
+    food="alu vorta"
+}
+//ternary
+let food = money >100 ? 'biriany' : 'alur vorta'
 
-// if(active === true){
-//     cssClass = 'active';
-// }
-// else{
-//     cssClass = 'inactive'
-// }
-// //ternaray
-// active ? display():hideDisplay();
-// const cssClass = active ? 'active' : 'inactive'
-// //
-// active && display()
-// active || display()
+if(active === true){
+    cssClass = 'active';
+}
+else{
+    cssClass = 'inactive'
+}
+//ternaray
+active ? display():hideDisplay();
+const cssClass = active ? 'active' : 'inactive'
+//
+active && display()
+active || display()
 
 //
 active = true
 const x = active && 4;
 const x = active && 4;
 console.log(x)
+//string to number
+
+const number = +'34';
+console.log(number);
+
+//number to srtring
+const numetoText = 34+ " ";
+
+//
+function add(num1 ,num2=45){
+    return num1+num2
+}
+add(45,63);
+
