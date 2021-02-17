58c38267ada25c4067c3da58e3ace7968db63aa7
RianIslam <rianislamdr@gmail.com>
2021-02-17 22:37:38 +0600

    object destrucring

--- JavaScript_Must_Need/destrucring_dot.js
+++ JavaScript_Must_Need/destrucring_dot.js
@@ -4,4 +4,11 @@
 // console.log(x,y)
 const numbers =[53,52,56,78];
 const [x,y] = numbers;
-console.log(x,y)
+// console.log(x,y)
+
+function boxify(num1){
+    return [num1,num1+4];
+
+}
+const [box1,box2] = boxify(5);
+console.log(box1,box2)
