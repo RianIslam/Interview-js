b6009b519ace99b997263247fc86c7182b71fa6c
RianIslam <rianislamdr@gmail.com>
2021-02-17 15:54:45 +0600

    array with map

--- JavaScript_Must_Need/array.js
+++ JavaScript_Must_Need/array.js
@@ -1,12 +1,13 @@
-const numbers = [3,56,97,56,967];
-const friends = ['rafiq','jabbar','barkat'];
+// const numbers = [3,56,97,56,967];
+// const friends = ['rafiq','jabbar','barkat'];
 const products = [
     {id:1,name:'laptop',price:8485},
     {id:2,name:'mobile',price:244},
     {id:3,name:'watch',price:533},
     {id:4,name:'tablet',price:245}
-];
+];  
 
 //map
 const names = products.map(product => product.name);
-console.log(names);
+const prices = products.map(pd => pd.price)
+console.log(prices);
