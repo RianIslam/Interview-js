c5680056b29d4383a205b27d1c6fe3b2d7bb2abb
RianIslam <rianislamdr@gmail.com>
2021-02-18 01:15:41 +0600

    shortcut

---
+++ JavaScript_Must_Need/shortCut.js
@@ -0,0 +1,29 @@
+// const money = 348;
+// const user =true;
+// let food;
+// if(money>100){
+//     food = 'biriany'
+// }else{
+//     food="alu vorta"
+// }
+// //ternary
+// let food = money >100 ? 'biriany' : 'alur vorta'
+
+// if(active === true){
+//     cssClass = 'active';
+// }
+// else{
+//     cssClass = 'inactive'
+// }
+// //ternaray
+// active ? display():hideDisplay();
+// const cssClass = active ? 'active' : 'inactive'
+// //
+// active && display()
+// active || display()
+
+//
+active = true
+const x = active && 4;
+const x = active && 4;
+console.log(x)
