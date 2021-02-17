c869a3cd91ad17dd905511e049b60f6d457cd1cb
RianIslam <rianislamdr@gmail.com>
2021-02-17 16:02:09 +0600

    forEach must need

--- JavaScript_Must_Need/array.js
+++ JavaScript_Must_Need/array.js
@@ -1,13 +1,14 @@
 // const numbers = [3,56,97,56,967];
 // const friends = ['rafiq','jabbar','barkat'];
 const products = [
-    {id:1,name:'laptop',price:8485},
-    {id:2,name:'mobile',price:244},
-    {id:3,name:'watch',price:533},
-    {id:4,name:'tablet',price:245}
+    {id: 1, name: 'laptop', price:8485},
+    {id: 2, name: 'mobile', price:244},
+    {id: 3, name: 'watch', price:533},
+    {id: 4, name: 'tablet', price:245}
 ];  
 
 //map
 const names = products.map(product => product.name);
 const prices = products.map(pd => pd.price)
-console.log(prices);
+// console.log(prices);
+products.forEach(product => console.log(product.name))
