9091ae015db9fed843550c2ebb929fab2cdfe87a
RianIslam <rianislamdr@gmail.com>
2021-02-17 19:24:31 +0600

    destrucring dot

--- JavaScript_Must_Need/destrucring_dot.js
+++ JavaScript_Must_Need/destrucring_dot.js
@@ -1,4 +1,7 @@
 //array destructing
-const numbers =[54,54];
-const [x,y] = [53,24];
+// const numbers =[54,54];
+// const [x,y] = [53,24];
+// console.log(x,y)
+const numbers =[53,52,56,78];
+const [x,y] = numbers;
 console.log(x,y)
