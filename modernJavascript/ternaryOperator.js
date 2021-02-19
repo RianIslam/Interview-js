183db6ed839c19fe8bfbc6c09261e7dee17ae4d6
RianIslam <rianislamdr@gmail.com>
2021-02-20 03:43:31 +0600

    add code

--- modernJavascript/ternaryOperator.js
+++ modernJavascript/ternaryOperator.js
@@ -1,9 +1,9 @@
-var age = 9;
-// if (age >= 18) {
-//   type = "adult";
-// } else {
-//   type = "child";
-// }
+var age = 8;
+if (age >= 18) {
+  type = "adult";
+} else {
+  type = "child";
+}
 
 var type = (age>=18) ? "adult" : (age<10) ? "child" : "young"
 console.log(type)
