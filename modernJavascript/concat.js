e0478228cf9ad6cb00b50546388c507c15c809ca
RianIslam <rianislamdr@gmail.com>
2021-02-20 04:23:23 +0600

    javascript push

---
+++ modernJavascript/concat.js
@@ -0,0 +1,5 @@
+var number1=[1,3,4,5,6,7]
+var number2 =[32,3,4,5,6,677,8]
+
+ var result = number1.concat(number2)
+ console.log(result)
---
+++ modernJavascript/push.js
@@ -0,0 +1,3 @@
+var number1 =[1,3,4,5,6,7]
+number1.push(4,5,6,7,8,98)
+console.log(number1)
