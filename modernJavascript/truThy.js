83d98761a213f51099facb29e05a2e87c0aa9ade
RianIslam <rianislamdr@gmail.com>
2021-02-20 03:11:54 +0600

    6 time i get falsy

--- modernJavascript/truThy.js
+++ modernJavascript/truThy.js
@@ -5,3 +5,22 @@ if (myVar){
 else{
     console.log("i am falsy")
 }
+
+// "" = falsy
+var myVar = "";
+if (myVar){
+    console.log("i am truthy")
+}
+else{
+    console.log("i am falsy")
+}
+
+//  if null,0,false,undefined,NaN = falsy
+
+var myVar = null;
+if (myVar){
+    console.log("i am truthy")
+}
+else{
+    console.log("i am falsy")
+}
