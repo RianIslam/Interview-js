d32ebf82d2217a24ac755cb3a2361ac956662176
RianIslam <rianislamdr@gmail.com>
2021-02-20 02:46:07 +0600

    arrow in window object browser

--- modernJavascript/this.js
+++ modernJavascript/this.js
@@ -1,11 +1,10 @@
-var javascript ={
-    name: "Javascript",
-    libraries:["React","Angular","Vue"],
-    printLibraries:function(){
-        
-        this.libraries.forEach((a)=>{
-            console.log(`${this.name} loves ${a}`)
-        })
-    }
-}
+var javascript = {
+  name: "Javascript",
+  libraries: ["React", "Angular", "Vue"],
+  printLibraries: function () {
+    this.libraries.forEach((a) => {
+      console.log(`${this.name} loves ${a}`);
+    });
+  },
+};
 javascript.printLibraries();
