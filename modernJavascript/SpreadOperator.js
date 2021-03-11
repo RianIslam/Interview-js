39ce63b517df4568b8aab100e8ace2e043f2df83
RianIslam <rianislamdr@gmail.com>
2021-03-12 02:09:36 +0600

    spread operator

--- modernJavascript/SpreadOperator.js
+++ modernJavascript/SpreadOperator.js
@@ -3,3 +3,21 @@ var numbers =[1,3,4,5,6,7]
 var newNumber = [...numbers, 1,3,4,5,6,7]
 
 console.log(newNumber)
+
+var one = [1,3,4,5,6,7]
+var two = [1,3,4,5,6,7]
+
+var wow = [...one,...two]
+var objOne = {
+    name:'branden Echi',
+    estb:'1'
+}
+var objTwo = {
+    name:'rian',
+    estb:'4'
+}
+console.log({
+    ...objOne,
+    ...objTwo
+})
+console.log(wow)
