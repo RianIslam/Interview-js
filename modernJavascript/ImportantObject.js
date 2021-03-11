00651d136b7143c13162e318133ff89cf29bd639
RianIslam <rianislamdr@gmail.com>
2021-03-12 01:12:09 +0600

    add object entries

--- modernJavascript/ImportantObject.js
+++ modernJavascript/ImportantObject.js
@@ -5,5 +5,9 @@ var myObj ={
     ranking:1
 };
 var keys = Object.keys(myObj)
+var values = Object.values(myObj)
+var entries = Object.entries(myObj)
+console.log(entries)
+console.log(values)
 console.log(keys)
 console.log(myObj);
