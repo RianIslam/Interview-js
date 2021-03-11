699d805b0e74ff09e6ff358797c834442848205d
RianIslam <rianislamdr@gmail.com>
2021-03-12 01:23:03 +0600

    objcet keys,values,entries

--- modernJavascript/ImportantObject.js
+++ modernJavascript/ImportantObject.js
@@ -1,8 +1,14 @@
+let x= 4;
+let y= 5;
+let z= x*y;
 var myObj ={
     name:'javascript',
     founder:'brendan Eich',
     estd:'1995',
-    ranking:1
+    ranking:1,
+    x,
+    y,
+    z
 };
 var keys = Object.keys(myObj)
 var values = Object.values(myObj)
